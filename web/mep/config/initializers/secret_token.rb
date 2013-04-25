3bcd67a58ea20efd4edd378fa285305ce05ad22c
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-04-25 20:52:32 -0300

    Add web proyect and android directory

---
+++ web/mep/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mep::Application.config.secret_token = '2ba31e9452bc57a2ab10cab4e353d4d74b788ccd33678a53a6bbc0d9dbb9be1f4af6f1631c9a438dbb366dd3226954a79bf8e0071724f1955c58e4b03e20869b'
