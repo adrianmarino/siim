161406e9b504fa84788aace6c20265ba96797374
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-04-27 10:59:57 -0300

    Adjust main layout

--- web/mep/app/models/user.rb
+++ web/mep/app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	# -------------------------------------------------------------------------
+	# Public Methods...
+	# -------------------------------------------------------------------------
+	def to_s
+		email
+	end
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+	# -------------------------------------------------------------------------
+	# Attributes...
+	# -------------------------------------------------------------------------
+	attr_accessible :email, :password, :password_confirmation, :remember_me
+
+	# -------------------------------------------------------------------------
+	# Authorization Attributes...
+	# -------------------------------------------------------------------------
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
