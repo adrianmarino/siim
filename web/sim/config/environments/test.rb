94994814296508fb1bb80dd52087e7bc9941d608
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-06-15 18:00:50 -0300

    Add mailer smpt config

--- web/sim/config/environments/test.rb
+++ web/sim/config/environments/test.rb
@@ -34,4 +34,15 @@ Sim::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'adrianmarino',
+    :password             => 'R2D2lv3jg6',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true 
+  }
 end
