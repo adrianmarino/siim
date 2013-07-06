d9ccec02a0d0720eb3dbcf88c22acf15d0494265
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-07-06 13:34:12 -0300

    CurdFormHelper: Refactor to simplify and suport nested fields

--- web/siim/core/app/models/crud_form_helper.rb
+++ web/siim/core/app/models/crud_form_helper.rb
@@ -1,4 +1,3 @@
-
 module CrudFormHelper
   # -------------------------------------------------------------------------
   # Public Methods...
@@ -11,23 +10,32 @@ module CrudFormHelper
     render :partial => 'crud/submit', :locals => {:f => a_form}
   end
 
-  def text(a_form, a_field_name)
-    render :partial => 'crud/text', :locals => {:f => a_form, :name => a_field_name}
+  # -------------------------------------------------------------------------
+  # Simple Fields...
+  # -------------------------------------------------------------------------
+
+  def text(a_form, a_field_name, an_entity_name = @entity_name)
+    render args(:text, a_form, a_field_name, an_entity_name)
   end
 
-  def state_assoc_select(a_form, a_field_name)
-    render :partial => 'crud/state_assoc', :locals => {:f => a_form, :name => a_field_name}
+  def datepicker(a_form, a_field_name, an_entity_name = @entity_name)
+    render args(:date, a_form, a_field_name, an_entity_name)
   end
 
-  def one_assoc_select(a_form, a_field_name, all_assoc_entities = nil )
-    render :partial => 'crud/one_assoc', :locals => {:f => a_form,  :name => a_field_name, :all => all_assoc_entities}
+  # -------------------------------------------------------------------------
+  # Association Fields...
+  # -------------------------------------------------------------------------
+
+  def one_select(a_form, a_field_name, an_entity_name = @entity_name )
+    render args(:one, a_form, a_field_name, an_entity_name)
   end
 
-  def many_assoc_select(a_form, a_field_name, all_assoc_entities, a_selected_ids)
-    render :partial => 'crud/many_assoc', :locals => {:f => a_form, :name => a_field_name, :all => all_assoc_entities, :selected_ids => a_selected_ids}
-  end
-
-  def datepicker(a_form, a_field_name)
-    render :partial => 'crud/date', :locals => {:f => a_form, :name => a_field_name}
-  end
+  # -------------------------------------------------------------------------
+  # Private Methods...
+  # -------------------------------------------------------------------------
+  private
+    def args(a_view, a_form, a_field_name, an_entity_name)
+      field_label = translate_attribute an_entity_name, a_field_name
+      {:partial => "crud/#{a_view}", :locals => {:form => a_form, :field_name => a_field_name, :field_label => field_label} }
+    end
 end
