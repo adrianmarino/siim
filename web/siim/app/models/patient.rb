90ab3b19c75d76f2f5f7cf8457d8d8359578acd3
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-07-05 23:36:37 -0300

    patinet.to_s

--- web/siim/app/models/patient.rb
+++ web/siim/app/models/patient.rb
@@ -1,5 +1,12 @@
 class Patient < ActiveRecord::Base
   # -------------------------------------------------------------------------
+  # Public Methods...
+  # -------------------------------------------------------------------------
+  def to_s
+    "#{@firstname} #{@lastname}"
+  end
+
+  # -------------------------------------------------------------------------
   # Attributes...
   # -------------------------------------------------------------------------
   attr_accessible :address, :birthdate, :blood_factor, :blood_group, :dni, :email, :firstname, :height, :home_phone, :lastname, :movile_phone, :sex, :weight
