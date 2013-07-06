77c25eb42dd4eb59cdf1370b80697bfa3e7e116c
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-07-05 23:26:41 -0300

    basic medical history CRUD config

--- web/siim/app/models/medical_history.rb
+++ web/siim/app/models/medical_history.rb
@@ -1,5 +1,10 @@
 class MedicalHistory < ActiveRecord::Base
   # -------------------------------------------------------------------------
+  # Attributes...
+  # -------------------------------------------------------------------------
+  attr_accessible :id
+
+  # -------------------------------------------------------------------------
   # Associations...
   # -------------------------------------------------------------------------
   has_many :allergies, :antecedents, :consutations, :diseases, :medical_exams, :medications, :vaccines
