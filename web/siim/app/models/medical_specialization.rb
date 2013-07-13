e4119e258294298a1183264b63d513c72ae302c5
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-07-13 15:06:04 -0300

    comparing methods

--- web/siim/app/models/medical_specialization.rb
+++ web/siim/app/models/medical_specialization.rb
@@ -1,9 +1,21 @@
 class MedicalSpecialization < ActiveRecord::Base
+  include Comparable
+
   # -------------------------------------------------------------------------
   # Public Methods...
   # -------------------------------------------------------------------------
   def to_s
     name
+  end
+
+  def <=>(an_other)
+    if self.to_s < an_other.to_s
+      -1
+    elsif self.to_s > an_other.to_s
+      1
+    else
+      0
+    end
   end
 
   # -------------------------------------------------------------------------
