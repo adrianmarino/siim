fad4a465cef2b7223a4e4340f6c64787b48eb39d
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-07-14 21:50:25 -0300

    fix sessions controller for get token

--- web/siim/app/controllers/application_controller.rb
+++ web/siim/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery
-  # before_filter :setup_locale, :authenticate_user!
+  protect_from_forgery
+  before_filter :setup_locale, :authenticate_user!
 
   def setup_locale
     I18n.locale = HTTPHeaderLocaleExtractor.new(request,logger).extract_locale
--- web/siim/app/controllers/sessions_controller.rb
+++ web/siim/app/controllers/sessions_controller.rb
@@ -1,30 +1,18 @@
 class SessionsController < Devise::SessionsController
-  before_filter :authenticate_user!, :except => [:create, :destroy]
-  respond_to :json
 
   def create
-    resource = User.find_for_database_authentication(:email => params[:email])
-    return invalid_login_attempt unless resource
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
+    resource.ensure_authentication_token!
 
-    if resource.valid_password?(params[:password])
-      sign_in(:user, resource)
-      resource.ensure_authentication_token!
-      render :json=> {:success=>true, :auth_token=>resource.authentication_token, :email=>resource.email}
-      return
+    respond_to do |format|
+      format.html do
+        respond_with resource, :location => after_sign_in_path_for(resource)
+      end
+      format.json do
+        render :json => { :response => 'ok', :auth_token => current_user.authentication_token }.to_json, :status => :ok
+      end
     end
-    invalid_login_attempt
-  end
-
-  def destroy
-    resource = User.find_for_database_authentication(:email => params[:email])
-    resource.authentication_token = nil
-    resource.save
-    render :json=> {:success=>true}
-  end
-
-  protected
-
-  def invalid_login_attempt
-    render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
   end
 end
