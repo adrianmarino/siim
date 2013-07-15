3c7f73caa02b7dfa98f26c1902eebcd72c4ecbcf
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-07-15 19:49:24 -0300

    fix home url

--- web/siim/config/environments/production.rb
+++ web/siim/config/environments/production.rb
@@ -65,5 +65,5 @@ Siim::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'sim.bambusolutions.com.ar' }
+  config.action_mailer.default_url_options = { :host => 'siim.bambusolutions.com.ar' }
 end
