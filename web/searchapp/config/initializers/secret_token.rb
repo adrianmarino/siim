068edf384b222bba0b91a2906aeabc8bbd61f7e6
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-08-19 17:14:08 -0300

    Remove elasticsearch

--- web/searchapp/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Searchapp::Application.config.secret_token = '1f84530a7f4b09a537e3683bb2a3c0db3a5d8bdd987dc1f3c17077c0be95f1673e74be4331d2ac2a39bc8b8e35cc0974792d771424a330a9a4ce15b1c7ba6ba3'
