f5060104bdf09d4a718559d2052d7c92aab89ce0
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-09-14 10:47:50 -0300

    add attention periods to seeds

--- web/siim/db/seeds.rb
+++ web/siim/db/seeds.rb
@@ -21,6 +21,34 @@ clinic = MedicalSpecialization.create(name: "Clinico")
 
 
 
+
+# -----------------------------------------------------------------------------
+# Attention Periods...
+# -----------------------------------------------------------------------------
+period_monday_a = AttentionPeriod.create(
+  begin_hour: 9, 
+  begin_minutes: 0,
+  end_hour: 13,
+  end_minutes: 0,
+  week_day: 1
+)
+period_monday_b = AttentionPeriod.create(
+  begin_hour: 14, 
+  begin_minutes: 0,
+  end_hour: 18,
+  end_minutes: 0,
+  week_day: 1
+)
+period_friday_c = AttentionPeriod.create(
+  begin_hour: 14, 
+  begin_minutes: 0,
+  end_hour: 20,
+  end_minutes: 0,
+  week_day: 5
+)
+
+
+
 # -----------------------------------------------------------------------------
 # Medicals...
 # -----------------------------------------------------------------------------
@@ -28,49 +56,57 @@ medical_a = Medical.create(
   dni: "10042902",									cuil: "20-10042902-2",
   firstname: "Juan Pedro", 					lastname: "Pepe",
   home_phone: "47935551", 					movile_phone: "154487766",
-  medical_specialization: clinic
+  medical_specialization: clinic,
+  attention_periods: [period_monday_a,period_monday_b]
 )
 medical_b = Medical.create(
   dni: "11042902",									cuil: "20-11042902-2",
   firstname: "Manuel", 							lastname: "Perez",
   home_phone: "47935551", 					movile_phone: "154487766",
-  medical_specialization: oftalmologo
+  medical_specialization: oftalmologo,
+  attention_periods: [period_monday_a,period_monday_b]
 )
 Medical.create(
   dni: "12042902",									cuil: "20-12042902-2",
   firstname: "Martin", 							lastname: "Gomez Gomenez",
   home_phone: "47935551", 					movile_phone: "154487766",
-  medical_specialization: traumatologo
+  medical_specialization: traumatologo,
+  attention_periods: [period_monday_a,period_monday_b]
 )
 Medical.create(
   dni: "13042902",									cuil: "20-13042902-2",
   firstname: "Adrian", 							lastname: "Marino",
   home_phone: "47935551", 					movile_phone: "154487766",
-  medical_specialization: pediatra
+  medical_specialization: pediatra,
+  attention_periods: [period_friday_c]
 )
 Medical.create(
   dni: "14042902",									cuil: "20-14042902-2",
   firstname: "Cristian", 						lastname: "Molina",
   home_phone: "47935551", 					movile_phone: "154487766",
-  medical_specialization: clinic
+  medical_specialization: clinic,
+  attention_periods: [period_monday_a,period_monday_b]
 )
 Medical.create(
   dni: "15042902",									cuil: "20-15042902-2",
   firstname: "Juan Pablo", 					lastname: "Ron",
   home_phone: "47935551", 					movile_phone: "154487766",
-  medical_specialization: oftalmologo
+  medical_specialization: oftalmologo,
+  attention_periods: [period_monday_a,period_monday_b]
 )
 Medical.create(
   dni: "16042902",									cuil: "20-16042902-2",
   firstname: "Felix", 							lastname: "Estrada",
   home_phone: "47935551", 					movile_phone: "154487766",
-  medical_specialization: traumatologo
+  medical_specialization: traumatologo,
+  attention_periods: [period_monday_a,period_monday_b]
 )
 Medical.create(
   dni: "17042902",									cuil: "20-17042902-2",
   firstname: "Daniel", 							lastname: "Grosso",
   home_phone: "47935551", 					movile_phone: "154487766",
-  medical_specialization: pediatra
+  medical_specialization: pediatra,
+  attention_periods: [period_friday_c]
 )
 
 
