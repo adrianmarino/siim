7ff3892e71a3d19b1087dfb376dfcc7c5d88338f
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-10-15 01:02:20 -0300

    Add index config

--- web/siim/app/controllers/medical_histories_controller.rb
+++ web/siim/app/controllers/medical_histories_controller.rb
@@ -1,143 +1,144 @@
 class MedicalHistoriesController < CrudController
-  # -------------------------------------------------------------------------
-  # Public Methods...
-  # -------------------------------------------------------------------------
-  # GET /medical_histories/search_by_dni?query="a DNI"
-  def search_by_dni
-    medical_history = MedicalHistory.find_by_dni dni_param
-    response = new_medical_history_response medical_history
-    render json: response.to_json
-  end
+	# -------------------------------------------------------------------------
+	# Public Methods...
+	# -------------------------------------------------------------------------
+	# GET /medical_histories/search_by_dni?query="a DNI"
+	def search_by_dni
+		medical_history = MedicalHistory.find_by_dni dni_param
+		response = new_medical_history_response medical_history
+		render json: response.to_json
+	end
 
-  def search
-    render search_medical_histories_path
-  end
+	def search
+		render search_medical_histories_path
+	end
 
-  def perform_search
-    # @medical_histories = MedicalHistory.all
-    search
-  end
+	def perform_search
+		@text = params[:text]
+		@medical_histories = MedicalHistory.search text
+		search
+	end
 
-  def custom_search
-    render custom_search_medical_histories_path
-  end
+	def custom_search
+		render custom_search_medical_histories_path
+	end
 
-  def perform_custom_search
-    @text = params[:text]
-    @medical_histories = MedicalHistory.all
-    custom_search
-  end
+	def perform_custom_search
+		@text = params[:text]
+		@medical_histories = MedicalHistory.all
+		custom_search
+	end
 
-  # -------------------------------------------------------------------------
-  # CRUD Methods...
-  # -------------------------------------------------------------------------
-  # GET /medical_histories
-  # GET /medical_histories.json
-  def index
-    @medical_histories = MedicalHistory.all
-    hide_view_action
-    hide_remove_action
+	# -------------------------------------------------------------------------
+	# CRUD Methods...
+	# -------------------------------------------------------------------------
+	# GET /medical_histories
+	# GET /medical_histories.json
+	def index
+		@medical_histories = MedicalHistory.all
+		hide_view_action
+		hide_remove_action
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @medical_histories }
-    end
-  end
+		respond_to do |format|
+			format.html # index.html.erb
+			format.json { render json: @medical_histories }
+		end
+	end
 
-  # GET /medical_histories/1
-  # GET /medical_histories/1.json
-  def show
-    @medical_history = MedicalHistory.find(params[:id])
+	# GET /medical_histories/1
+	# GET /medical_histories/1.json
+	def show
+		@medical_history = MedicalHistory.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @medical_history }
-    end
-  end
+		respond_to do |format|
+			format.html # show.html.erb
+			format.json { render json: @medical_history }
+		end
+	end
 
-  # GET /medical_histories/new
-  # GET /medical_histories/new.json
-  def new
-    @medical_history = MedicalHistory.new
-    @medical_history.initialize_with_associations
-    @medicals = Medical.all.sort
+	# GET /medical_histories/new
+	# GET /medical_histories/new.json
+	def new
+		@medical_history = MedicalHistory.new
+		@medical_history.initialize_with_associations
+		@medicals = Medical.all.sort
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @medical_history }
-    end
-  end
+		respond_to do |format|
+			format.html # new.html.erb
+			format.json { render json: @medical_history }
+		end
+	end
 
-  # GET /medical_histories/1/edit
-  def edit
-    @medical_history = MedicalHistory.find(params[:id])
-    @medicals = Medical.all.sort
-    @back_url = params[:back_url].nil? ? medical_histories_path : params[:back_url]
-  end
+	# GET /medical_histories/1/edit
+	def edit
+		@medical_history = MedicalHistory.find(params[:id])
+		@medicals = Medical.all.sort
+		@back_url = params[:back_url].nil? ? medical_histories_path : params[:back_url]
+	end
 
-  # POST /medical_histories
-  # POST /medical_histories.json
-  def create
-    @medical_history = MedicalHistory.new(params[:medical_history])
-    @medicals = Medical.all.sort
-    respond_to do |format|
-      if @medical_history.save
-        format.html { redirect_to medical_histories_path, notice: CrudTranslations.model_was_created(@medical_history) }
-        format.json { render json: @medical_history, status: :created, location: @medical_history }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @medical_history.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# POST /medical_histories
+	# POST /medical_histories.json
+	def create
+		@medical_history = MedicalHistory.new(params[:medical_history])
+		@medicals = Medical.all.sort
+		respond_to do |format|
+			if @medical_history.save
+				format.html { redirect_to medical_histories_path, notice: CrudTranslations.model_was_created(@medical_history) }
+				format.json { render json: @medical_history, status: :created, location: @medical_history }
+			else
+				format.html { render action: "new" }
+				format.json { render json: @medical_history.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PUT /medical_histories/1
-  # PUT /medical_histories/1.json
-  def update
-    @medical_history = MedicalHistory.find(params[:id])
-    @medicals = Medical.all.sort
-    @back_url = params[:back_url].nil? ? medical_histories_path : params[:back_url]
-    respond_to do |format|
-      if @medical_history.update_attributes(params[:medical_history])
-        format.html { redirect_to @back_url, notice: CrudTranslations.model_was_updated(@medical_history) }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @medical_history.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# PUT /medical_histories/1
+	# PUT /medical_histories/1.json
+	def update
+		@medical_history = MedicalHistory.find(params[:id])
+		@medicals = Medical.all.sort
+		@back_url = params[:back_url].nil? ? medical_histories_path : params[:back_url]
+		respond_to do |format|
+			if @medical_history.update_attributes(params[:medical_history])
+				format.html { redirect_to @back_url, notice: CrudTranslations.model_was_updated(@medical_history) }
+				format.json { head :no_content }
+			else
+				format.html { render action: "edit" }
+				format.json { render json: @medical_history.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # DELETE /medical_histories/1
-  # DELETE /medical_histories/1.json
-  def destroy
-    @medical_history = MedicalHistory.find(params[:id])
-    @medical_history.destroy
+	# DELETE /medical_histories/1
+	# DELETE /medical_histories/1.json
+	def destroy
+		@medical_history = MedicalHistory.find(params[:id])
+		@medical_history.destroy
 
-    respond_to do |format|
-      format.html { redirect_to medical_histories_url }
-      format.json { head :no_content }
-    end
-  end
+		respond_to do |format|
+			format.html { redirect_to medical_histories_url }
+			format.json { head :no_content }
+		end
+	end
 
-  # -------------------------------------------------------------------------
-  # Private Methods...
-  # -------------------------------------------------------------------------
-  private
-  def dni_param
-    dni = params[:query]
-  end
+	# -------------------------------------------------------------------------
+	# Private Methods...
+	# -------------------------------------------------------------------------
+	private
+	def dni_param
+		dni = params[:query]
+	end
 
-  def new_medical_history_response(a_medical_history)
-    MedicalHistoryResponse.new a_medical_history
-  end
+	def new_medical_history_response(a_medical_history)
+		MedicalHistoryResponse.new a_medical_history
+	end
 
-  # -------------------------------------------------------------------------
-  # Constructor...
-  # -------------------------------------------------------------------------
-  def initialize
-    super
-    @entity_name = 'medical_history'
-    @plural_entity_name = 'medical_histories'
-  end
+	# -------------------------------------------------------------------------
+	# Constructor...
+	# -------------------------------------------------------------------------
+	def initialize
+		super
+		@entity_name = 'medical_history'
+		@plural_entity_name = 'medical_histories'
+	end
 end
--- web/siim/app/models/medical_history.rb
+++ web/siim/app/models/medical_history.rb
@@ -1,74 +1,200 @@
 class MedicalHistory < ActiveRecord::Base
-  # -------------------------------------------------------------------------
-  # Public Methods...
-  # -------------------------------------------------------------------------
-  def initialize_with_associations
-    a_patient = Patient.new
-    a_patient.medical_history = self
-    self.patient = a_patient
-  end
+	include Tire::Model::Search
+	include Tire::Model::Callbacks
 
-  def as_json(options={})
-    super(
-      :only => [:id],
-      :include =>  {
-        :patient => { :only => [:dni, :firstname, :lastname, :birthdate, :blood_type,
-                                :height, :weight, :sex, :address, :email, :home_phone, :movile_phone] 
-                    },
-        :allergies => {:only =>[:cause]},
-        :antecedents => { :only => [:description]},
-        :consultations => {:only=>[:diagnostic]},
-        :diseases => {:only =>[:name]},
-        :medications => {:only => [:name,:dose,:how_often]},
-        :vaccines => {:only => [:last_application, :name]},
-        :medical_exams => {:only => [:achievement_date, :name]}
-      }
-    )
-  end
+	# -------------------------------------------------------------------------
+	# Public Methods...
+	# -------------------------------------------------------------------------
+	def initialize_with_associations
+		a_patient = Patient.new
+		a_patient.medical_history = self
+		self.patient = a_patient
+	end
 
-  # -------------------------------------------------------------------------
-  # Public Class Methods...
-  # -------------------------------------------------------------------------
-  def self.find_by_dni(a_dni)
-    MedicalHistory.includes(:patient).where("patients.dni=?",a_dni).first
-  end
+	def as_json(options={})
+		super(
+			:only => [:id],
+			:include =>	{
+				:patient => { :only => [:dni, :firstname, :lastname, :birthdate, :blood_type,
+																:height, :weight, :sex, :address, :email, :home_phone, :movile_phone] 
+										},
+				:allergies => {:only =>[:cause]},
+				:antecedents => { :only => [:description]},
+				:consultations => {:only=>[:diagnostic]},
+				:diseases => {:only =>[:name]},
+				:medications => {:only => [:name,:dose,:how_often]},
+				:vaccines => {:only => [:last_application, :name]},
+				:medical_exams => {:only => [:achievement_date, :name]}
+			}
+		)
+	end
 
-  def self.find_by_patient(a_patient)
-    MedicalHistory.includes(:patient).where("patients.id=?",a_patient.id).first
-  end
+	def to_indexed_json
+		as_json
+	end
 
-  # -------------------------------------------------------------------------
-  # Attributes...
-  # -------------------------------------------------------------------------
-  attr_accessible :id, :allergies, :antecedents, :consultations, :diseases,
-    :medical_exams, :medications, :vaccines, :patient, :allergies_attributes,
-    :antecedents_attributes, :consultations_attributes, :diseases_attributes,
-    :medical_exams_attributes, :medications_attributes, :vaccines_attributes,
-    :patient_attributes, :_destroy
+	def self.search(a_text) 
+		if a_text.empty?
+			@results = self.all 
+		else
+			content_query = lambda do |should|
+				should.string "content:#{a_text}" # , :boost => 5, :type => "phrase"
+			end
+			title_query = lambda do |should|
+				should.string "title:#{a_text}" # , :boost => 5, :type => "phrase"
+			end
+			search = Tire.search 'results' do
+				query do
+					boolean do
+						should &content_query
+						should &title_query
+					end
+				end
+				highlight :title, :content, :options => { :tag => '<strong class="highlight">' }
+			end
+			@results = search.results
+		end
+		@results
+	end
 
-  attr_accessor :_destroy
+	# -------------------------------------------------------------------------
+	# Public Class Methods...
+	# -------------------------------------------------------------------------
+	def self.find_by_dni(a_dni)
+		MedicalHistory.includes(:patient).where("patients.dni=?",a_dni).first
+	end
 
-  # -------------------------------------------------------------------------
-  # Associations...
-  # -------------------------------------------------------------------------
-  has_many  :allergies, dependent: :delete_all
-  has_many  :antecedents, dependent: :delete_all
-  has_many  :consultations, dependent: :delete_all
-  has_many  :diseases, dependent: :delete_all
-  has_many  :medical_exams, dependent: :delete_all
-  has_many  :medications, dependent: :delete_all
-  has_many  :vaccines, dependent: :delete_all
-  has_one   :patient, dependent: :delete
+	def self.find_by_patient(a_patient)
+		MedicalHistory.includes(:patient).where("patients.id=?",a_patient.id).first
+	end
 
-  # -------------------------------------------------------------------------
-  # Nested attributes...
-  # -------------------------------------------------------------------------
-  accepts_nested_attributes_for :allergies, allow_destroy: true
-  accepts_nested_attributes_for :antecedents, allow_destroy: true
-  accepts_nested_attributes_for :consultations, allow_destroy: true
-  accepts_nested_attributes_for :diseases, allow_destroy: true
-  accepts_nested_attributes_for :medical_exams, allow_destroy: true
-  accepts_nested_attributes_for :medications, allow_destroy: true
-  accepts_nested_attributes_for :vaccines, allow_destroy: true
-  accepts_nested_attributes_for :patient, allow_destroy: true
+	# -------------------------------------------------------------------------
+	# Attributes...
+	# -------------------------------------------------------------------------
+	attr_accessible :id, :allergies, :antecedents, :consultations, :diseases,
+		:medical_exams, :medications, :vaccines, :patient, :allergies_attributes,
+		:antecedents_attributes, :consultations_attributes, :diseases_attributes,
+		:medical_exams_attributes, :medications_attributes, :vaccines_attributes,
+		:patient_attributes, :_destroy
+
+	attr_accessor :_destroy
+
+	# -------------------------------------------------------------------------
+	# Associations...
+	# -------------------------------------------------------------------------
+	has_many	:allergies, dependent: :delete_all
+	has_many	:antecedents, dependent: :delete_all
+	has_many	:consultations, dependent: :delete_all
+	has_many	:diseases, dependent: :delete_all
+	has_many	:medical_exams, dependent: :delete_all
+	has_many	:medications, dependent: :delete_all
+	has_many	:vaccines, dependent: :delete_all
+	has_one	 :patient, dependent: :delete
+
+	# -------------------------------------------------------------------------
+	# Nested attributes...
+	# -------------------------------------------------------------------------
+	accepts_nested_attributes_for :allergies, allow_destroy: true
+	accepts_nested_attributes_for :antecedents, allow_destroy: true
+	accepts_nested_attributes_for :consultations, allow_destroy: true
+	accepts_nested_attributes_for :diseases, allow_destroy: true
+	accepts_nested_attributes_for :medical_exams, allow_destroy: true
+	accepts_nested_attributes_for :medications, allow_destroy: true
+	accepts_nested_attributes_for :vaccines, allow_destroy: true
+	accepts_nested_attributes_for :patient, allow_destroy: true
+
+	# -------------------------------------------------------------------------
+	# Search Settings...
+	# -------------------------------------------------------------------------
+	after_touch() { tire.update_index }
+
+	settings :number_of_shards => 1,
+					 :number_of_replicas => 1,
+					 :analysis => {
+						 :filter => {
+								:spanish_stop	=> {
+									'type'					 => 'stop',
+									'stopwords_path' => 'spanish_stop_words.txt'
+								},
+								:name_ngrams => {
+									'type'			=> 'edgeNGram',
+									'side'			=> 'front',
+									'max_gram'	=> 50,
+									'min_gram'	=> 2
+								},
+								:name_ngrams_back => {
+									 'side'		 => 'back',
+									 'max_gram' => 50,
+									 'min_gram' => 2,
+									 'type'		 => 'edgeNGram'
+								},
+								:name_middle_ngrams => {
+									 'type'		 => 'nGram',
+									 'max_gram' => 50,
+									 'min_gram' => 2
+								}
+						 },
+						 :analyzer => {
+								:full_name => {
+									'filter'		=> [ 'standard', 'lowercase','asciifolding','spanish_stop'],
+									'type'			=> 'custom',
+									'tokenizer' => 'standard'
+								},
+								:partial_name => {
+									'type'			=> 'custom',
+									'tokenizer' => 'standard',
+									'filter'		=> ['standard','lowercase','asciifolding','name_ngrams','spanish_stop']		 
+								},
+								:partial_name_back => {
+									'type'			 => 'custom',
+									'tokenizer'	=> 'standard',
+									'filter'		 => ['standard','lowercase','asciifolding','name_ngrams_back','spanish_stop']
+								},
+								:partial_middle_name => {
+									'type' =>'custom',
+									'tokenizer' => 'standard',
+									'filter' => ['standard','lowercase','asciifolding','name_middle_ngrams','spanish_stop']
+								}
+							 }
+						} do
+				mapping do
+					indexes :patient do
+						indexes :firstname, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :lastname, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :birthdate, :type => 'date', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :height, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :weight, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+					end
+					indexes :allergies do
+						indexes :cause, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :observations, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+					end
+					indexes :antecedents do
+						indexes :description, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+					end
+					indexes :consultations do
+						indexes :diagnostic, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :symptomps, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :treatment, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+					end
+					indexes :diseases do
+						indexes :name, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :observations, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+					end
+					indexes :medications do
+						indexes :name, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :route, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :dose, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :how_often, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+					end
+					indexes :vaccines do
+						indexes :name, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+					end
+					indexes :medical_exams do
+						indexes :name, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :results, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+						indexes :observations, :type => 'string', :index_analyzer => 'partial_middle_name' ,:search_analyzer => 'full_name'
+					end
+			end
+	end
 end
