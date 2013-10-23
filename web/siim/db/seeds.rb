91bec17cf472dcfabfda80f68fc98cc5e4fd213f
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-10-22 22:19:17 -0300

    Fix class name

--- web/siim/db/seeds.rb
+++ web/siim/db/seeds.rb
@@ -526,5 +526,5 @@ MedicalHistorySearchEngine.refresh_indexes
 # -----------------------------------------------------------------------------
 # Generate appointment window (Only for porduction deploy)...
 # -----------------------------------------------------------------------------
-AppointmentGeneratiorTask.new.run if Rails.env == 'production'
+AppointmentGeneratorTask.new.run if Rails.env == 'production'
 # -----------------------------------------------------------------------------
