a06012ec56a49860b47c34954af242d633f18448
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-10-25 21:41:47 -0300

    Custom search: Fix not show any highligh fields in the results

--- web/siim/app/models/highlight_wrapper.rb
+++ web/siim/app/models/highlight_wrapper.rb
@@ -4,29 +4,32 @@ class HighlightWrapper
 	# -------------------------------------------------------------------------
 	def value_of(a_field_name, a_frase)
 		if exist_highlight_for(a_field_name)
+			puts "#{a_field_name} :#{a_frase}. TIENE Highlight"
 			value = highlight_of(a_field_name).select do|a_highlihted_value|
+				puts "a_highlihted_value :#{a_highlihted_value}"
 				a_frase == remove_highlight_to(a_highlihted_value)
 			end
-			puts "VALUE :#{value.first}"
-			value.first
+			puts "VALUE :#{value}"
+			value.empty? ? a_frase : value.first
 		else
 			a_frase
 		end
 	end
 
-	def self.new_with_default_tags(a_highlight)
-		self.new a_highlight, APP_CONFIG.highlight_begin, APP_CONFIG.highlight_end
+	def self.new_with_default_tags(a_highlight, indexes)
+		self.new a_highlight, APP_CONFIG.highlight_begin, APP_CONFIG.highlight_end, indexes
 	end
 	# -------------------------------------------------------------------------
 	# Private Methods...
 	# -------------------------------------------------------------------------
 	private
 	def highlight_of(a_field_name)
-		@highlight[a_field_name]
+		values = (@indexes.collect {|an_index| @highlight["#{a_field_name}.#{an_index}"] }).select {|a_value| not a_value.nil? }
+		values = values.collect {|a_value| a_value[0] }
 	end
 
 	def exist_highlight_for(a_field_name)
-		not @highlight.nil? and not highlight_of(a_field_name).nil?
+		not @highlight.nil? and not highlight_of(a_field_name).empty?
 	end
 
 	def remove_highlight_to(a_value)
@@ -41,9 +44,10 @@ class HighlightWrapper
 	# -------------------------------------------------------------------------
 	# Initialize...
 	# -------------------------------------------------------------------------
-	def initialize(a_highlight, a_begin_tag, an_end_tag)
+	def initialize(a_highlight, a_begin_tag, an_end_tag, indexes)
 		@highlight = a_highlight
 		@begin_tag = a_begin_tag
 		@end_tag = an_end_tag
+		@indexes = indexes
 	end
 end
--- web/siim/app/models/medical_history_string_result_builder.rb
+++ web/siim/app/models/medical_history_string_result_builder.rb
@@ -3,29 +3,29 @@ class MedicalHistoryStringResultBuilder
 	# Public Methods...
 	# -------------------------------------------------------------------------
 	def with_patient
-		@result+= "#{patient_firstname}, #{patient_lastname}, #{patient_birthdate}, #{patient_height}, #{patient_weight}"
+		@result+= "#{patient_firstname}#{SEPARATOR}#{patient_lastname}#{SEPARATOR}#{patient_birthdate}#{SEPARATOR}#{patient_height}#{SEPARATOR}#{patient_weight}"
 		self
 	end
 
 	def with_allergies
 		@result = @medical_history.allergies.inject(@result) {|result, an_allergy |
-			result+=  ", #{allergy_cause(an_allergy)}"
-			result+= ", #{allergy_observations(an_allergy)}" if !an_allergy.observations.nil? and !an_allergy.observations.empty?
+			result+=  "#{SEPARATOR}#{allergy_cause(an_allergy)}"
+			result+= "#{SEPARATOR}#{allergy_observations(an_allergy)}" if !an_allergy.observations.nil? and !an_allergy.observations.empty?
 			result
 		}
 		self
 	end
 
 	def with_antecedents
-		@result = @medical_history.antecedents.inject(@result) {|result, an_antecedent | "#{result},#{an_antecedent.description}"}
+		@result = @medical_history.antecedents.inject(@result) {|result, an_antecedent | "#{result}#{SEPARATOR}#{antecedents_description(an_antecedent)}"}
 		self
 	end
 
 	def with_consultations
 		@result = @medical_history.consultations.inject(@result) {|result, a_consultation |
-			result+=  " ,#{consultation_diagnostic(a_consultation)}"
-			result+= ", #{consultation_symptomps(a_consultation)}" unless a_consultation.symptomps.nil? or a_consultation.symptomps.empty?
-			result+= ", #{consultation_treatment(a_consultation)}" unless a_consultation.treatment.nil? or a_consultation.treatment.empty?
+			result+=  " #{SEPARATOR}#{consultation_diagnostic(a_consultation)}"
+			result+= "#{SEPARATOR}#{consultation_symptomps(a_consultation)}" unless a_consultation.symptomps.nil? or a_consultation.symptomps.empty?
+			result+= "#{SEPARATOR}#{consultation_treatment(a_consultation)}" unless a_consultation.treatment.nil? or a_consultation.treatment.empty?
 			result
 		}
 		self
@@ -33,8 +33,8 @@ class MedicalHistoryStringResultBuilder
 
 	def with_diseases
 		@result = @medical_history.diseases.inject(@result) {|result, a_disease |
-			result+=  ", #{disease_name(a_disease)}"
-			result+= ", #{disease_observations(a_disease)}" unless a_disease.observations.nil? or a_disease.observations.empty?
+			result+= "#{SEPARATOR}#{disease_name(a_disease)}"
+			result+= "#{SEPARATOR}#{disease_observations(a_disease)}" unless a_disease.observations.nil? or a_disease.observations.empty?
 			result
 		}
 		self
@@ -42,22 +42,22 @@ class MedicalHistoryStringResultBuilder
 
 	def with_medications
 		@result = @medical_history.medications.inject(@result) {|result, a_medication |
-			result+=  ", #{medication_name(a_medication)}, #{medication_dose(a_medication)}, #{medication_amount(a_medication)}"
-			result+= ", #{medication_route(a_medication)}" unless a_medication.route.nil? or a_medication.route.empty?
+			result+= "#{SEPARATOR}#{medication_name(a_medication)}#{SEPARATOR}#{medication_dose(a_medication)}#{SEPARATOR}#{medication_amount(a_medication)}"
+			result+= "#{SEPARATOR}#{medication_route(a_medication)}" unless a_medication.route.nil? or a_medication.route.empty?
 			result
 		}
 		self
 	end
 
 	def with_vaccines
-		@result = @medical_history.vaccines.inject(@result) {|result, a_vaccine | "#{result}, #{a_vaccine.name}"}
+		@result = @medical_history.vaccines.inject(@result) {|result, a_vaccine |"#{result}#{SEPARATOR}#{vaccines_name(a_vaccine)}"}
 		self
 	end
 
 	def with_exams
 		@result = @medical_history.medical_exams.inject(@result) {|result, an_exam |
-			result+= ", #{exam_name(an_exam)}, #{exam_results(an_exam)}"
-			result+= ", #{exam_observations(an_exam)}" unless an_exam.observations.nil? or an_exam.observations.empty?
+			result+= "#{SEPARATOR}#{exam_name(an_exam)}#{SEPARATOR}#{exam_results(an_exam)}"
+			result+= "#{SEPARATOR}#{exam_observations(an_exam)}" unless an_exam.observations.nil? or an_exam.observations.empty?
 			result
 		}
 		self
@@ -68,7 +68,7 @@ class MedicalHistoryStringResultBuilder
 	end
 
 	def to_s
-		"...#{@result}..."
+		@result
 	end
 
 	# -------------------------------------------------------------------------
@@ -76,79 +76,87 @@ class MedicalHistoryStringResultBuilder
 	# -------------------------------------------------------------------------
 	private
 	def patient_firstname
-		value_of "patient.firstname.title", @medical_history.patient.firstname
+		value_of "patient.firstname", @medical_history.patient.firstname
 	end
 
 	def patient_lastname
-		value_of "patient.lastname.title", @medical_history.patient.lastname
+		value_of "patient.lastname", @medical_history.patient.lastname
 	end
 
 	def patient_birthdate
-		value_of "patient.birthdate.title", @medical_history.patient.birthdate
+		value_of "patient.birthdate", @medical_history.patient.birthdate
 	end
 
 	def patient_height
-		value_of "patient.height.title", @medical_history.patient.height
+		value_of "patient.height", @medical_history.patient.height
 	end
 
 	def patient_weight
-		value_of "patient.weight.title", @medical_history.patient.weight
+		value_of "patient.weight", @medical_history.patient.weight
 	end
 
 	def allergy_cause(an_model)
-		value_of "allergies.cause.title", an_model.cause
+		value_of "allergies.cause", an_model.cause
 	end
 
 	def allergy_observations(an_model)
-		value_of "allergies.observations.title", an_model.observations
+		value_of "allergies.observations", an_model.observations
 	end
 
 	def consultation_treatment(a_model)
-		value_of "consultations.treatment.title", a_model.treatment
+		value_of "consultations.treatment", a_model.treatment
 	end
 
 	def consultation_symptomps(a_model)
-		value_of "consultations.symptomps.title", a_model.symptomps
+		value_of "consultations.symptomps", a_model.symptomps
 	end
 
 	def consultation_diagnostic(a_model)
-		value_of "consultations.diagnostic.title", a_model.diagnostic
+		value_of "consultations.diagnostic", a_model.diagnostic
 	end
 
 	def disease_name(a_model)
-		value_of "diseases.name.title", a_model.name
+		value_of "diseases.name", a_model.name
 	end
 
 	def disease_observations(a_model)
-		value_of "diseases.observations.title", a_model.observations
+		value_of "diseases.observations", a_model.observations
 	end
 
 	def medication_name(a_model)
-		value_of "medications.name.title", a_model.name
+		value_of "medications.name", a_model.name
 	end
 
 	def medication_dose(a_model)
-		value_of "medications.dose.title", a_model.dose
+		value_of "medications.dose", a_model.dose
 	end
 
 	def medication_amount(a_model)
-		value_of "medications.amount.title", a_model.amount
+		value_of "medications.amount", a_model.amount
 	end
 
 	def medication_route(a_model)
-		value_of "medications.route.title", a_model.route
+		value_of "medications.route", a_model.route
 	end
 
 	def exam_name(a_model)
-		value_of "medical_exams.name.title", a_model.name
+		value_of "medical_exams.name", a_model.name
 	end
 
 	def exam_results(a_model)
-		value_of "medical_exams.results.title", a_model.results
+		value_of "medical_exams.results", a_model.results
 	end
 
 	def exam_observations(a_model)
-		value_of "medical_exams.observations.title", a_model.observations
+		value_of "medical_exams.observations", a_model.observations
+	end
+
+	def antecedents_description(a_model)
+		value_of "antecedents.description", a_model.description
+	end
+
+	def vaccines_name(a_model)
+		value_of "vaccines.name", a_model.name
 	end
 
 	def value_of(a_field_name, a_value)
@@ -162,9 +170,14 @@ class MedicalHistoryStringResultBuilder
 	# -------------------------------------------------------------------------
 	# Initialize...
 	# -------------------------------------------------------------------------
-	def initialize(a_medical_history)
+	def initialize(a_medical_history, indexes)
 		@medical_history = a_medical_history
 		@result = ""
-		@highlight_wrapper  = HighlightWrapper.new_with_default_tags @medical_history.highlight
+		@highlight_wrapper  = HighlightWrapper.new_with_default_tags @medical_history.highlight , indexes
 	end
+
+	# ----------------------------------------------------------------------------
+	# Constants
+	# ----------------------------------------------------------------------------
+	SEPARATOR = " ... "
 end
