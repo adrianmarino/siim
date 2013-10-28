ecbae0ab1ef0443b187465b87fd2eabab2139dd8
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-10-27 22:58:10 -0300

    Medical histoey show view for custom search.

--- web/siim/app/assets/javascripts/application.js
+++ web/siim/app/assets/javascripts/application.js
@@ -47,7 +47,7 @@ $.fn.datepicker.dates['es'] = {
 $(document).on("focus", "[data-behaviour~='datepicker']", function(e){
     $(this).datepicker({
         "format": "yyyy-mm-dd",
-        "weekStart": 1, 
+        "weekStart": 1,
         "autoclose": true,
         "language": I18n.locale
     });
