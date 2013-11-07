2adc9ee0a00d5b08d19558743e68e4bbae380c2c
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-11-06 23:33:40 -0300

    Comment incomplete validation

--- web/siim/app/models/user.rb
+++ web/siim/app/models/user.rb
@@ -52,7 +52,7 @@ class User < ActiveRecord::Base
 	# Validations...
 	# -------------------------------------------------------------------------
 	validates :dni, length: { minimum: 7, maximum: 10 }, :uniqueness => true, :presence => true
-	validates :home_phone, :movile_phone, length: { maximum: 20 } 
+	#validates :home_phone, :movile_phone, length: { maximum: 20 } 
 	#validates :home_phone, :movile_phone, :numericality => true 
 	validates :first_name, :last_name, length: { maximum: 30 }, :presence => true 
 	validates :email, :presence => true, :email => true
