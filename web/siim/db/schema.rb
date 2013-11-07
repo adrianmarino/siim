623fed421bd7e17de10d043a5cc510b3ea6209c2
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-11-07 20:30:16 -0300

    update schema

--- web/siim/db/schema.rb
+++ web/siim/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131102154000) do
+ActiveRecord::Schema.define(:version => 20131106224525) do
 
   create_table "allergies", :force => true do |t|
     t.string   "cause"
@@ -161,6 +162,8 @@ ActiveRecord::Schema.define(:version => 20131102154000) do
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
   end
+
+  add_index "patients", ["dni"], :name => "index_patients_on_dni"
 
   create_table "roles", :force => true do |t|
     t.string   "name"
