e5e24f0a1bf09ef4a6e2d1798adf407dfe91b419
Adrian Norberto Marino <adrianmarino@gmail.com>
2014-01-12 13:30:27 -0300

    Format

--- web/siim/app/controllers/application_controller.rb
+++ web/siim/app/controllers/application_controller.rb
@@ -20,19 +20,20 @@ class ApplicationController < ActionController::Base
 		I18n.locale = HTTPHeaderLocaleExtractor.new(request,logger).extract_locale
 	end
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit( :dni, :email, :first_name, :last_name, :birthdate,
-                :sex, :address, :movile_phone, :home_phone, :photo, :password,
-                :password_confirmation)
-    end
 
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:dni, :email, :first_name, :last_name, :birthdate,
-               :sex, :address, :movile_phone, :home_phone, :photo, :password,
-               :password_confirmation, :password, :password_confirmation,
-               :current_password)
-    end
-  end
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.for(:sign_up) do |u|
+			u.permit( :dni, :email, :first_name, :last_name, :birthdate,
+								:sex, :address, :movile_phone, :home_phone, :photo, :password,
+								:password_confirmation)
+		end
+
+		devise_parameter_sanitizer.for(:account_update) do |u|
+			u.permit(:dni, :email, :first_name, :last_name, :birthdate,
+							 :sex, :address, :movile_phone, :home_phone, :photo, :password,
+							 :password_confirmation, :password, :password_confirmation,
+							 :current_password)
+		end
+	end
 end
